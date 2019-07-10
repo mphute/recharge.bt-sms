26f673ffcb0819aeff9234731afc65357ab2a80d
mphute <49985696+mphute@users.noreply.github.com>
2019-07-10 08:31:43 +0530

    Update MyReceiver.java

--- MyReceiver.java
+++ MyReceiver.java
@@ -3,55 +3,55 @@ package edu.vit.eddv2;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.Telephony;
-import android.support.v4.content.ContextCompat;
 import android.telephony.SmsMessage;
+import android.telephony.SmsManager;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MyReceiver extends BroadcastReceiver {
-
-    private Bundle bundle;
-    private SmsMessage currentSMS;
-    private String message;
+    // Get the object of SmsManager
+    final SmsManager sms = SmsManager.getDefault();
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "In on recieve", Toast.LENGTH_SHORT).show();
 
-        if (intent.getAction().equals("Telephony.Sms.Intents.SMS_RECEIVED_ACTION")) {
-            bundle = intent.getExtras();
+        Toast.makeText(context,"In onrecieve",Toast.LENGTH_LONG).show();
+
+        // Retrieves a map of extended data from the intent.
+        final Bundle bundle = intent.getExtras();
+
+        try {
+
             if (bundle != null) {
-                Object[] pdu_Objects = (Object[]) bundle.get("pdus");
-                if (pdu_Objects != null) {
 
-                    for (Object aObject : pdu_Objects) {
+                final Object[] pdusObj = (Object[]) bundle.get("pdus");
 
-                        currentSMS = getIncomingMessage(aObject, bundle);
+                for (int i = 0; i < pdusObj.length; i++) {
 
-                        String senderNo = currentSMS.getDisplayOriginatingAddress();
+                    SmsMessage currentMessage = SmsMessage.createFromPdu((byte[]) pdusObj[i]);
+                    String phoneNumber = currentMessage.getDisplayOriginatingAddress();
 
-                        message = currentSMS.getDisplayMessageBody();
-                        Toast.makeText(context, "senderNum: " + senderNo + " :\n message: " + message, Toast.LENGTH_LONG).show();
-                    }
-                    // End of loop
-                }
-            }
-        } // bundle null
-    }
+                    String senderNum = phoneNumber;
+                    String message = currentMessage.getDisplayMessageBody();
+
+                     Log.i("SmsReceiver", "senderNum: "+ senderNum + "; message: " + message);
 
 
-    private SmsMessage getIncomingMessage(Object aObject, Bundle bundle) {
-        SmsMessage currentSMS;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            String format = bundle.getString("format");
-            currentSMS = SmsMessage.createFromPdu((byte[]) aObject, format);
-        } else {
-            currentSMS = SmsMessage.createFromPdu((byte[]) aObject);
+                    // Show Alert
+                    int duration = Toast.LENGTH_LONG;
+                    Toast toast = Toast.makeText(context,
+                            "senderNum: "+ senderNum + ", message: " + message, duration);
+                    toast.show();
+
+                } // end for loop
+            } // bundle is null
+
+
+
+        } catch (Exception e) {
+            Log.e("SmsReceiver", "Exception smsReceiver" +e);
+
         }
-        return currentSMS;
     }
 }
